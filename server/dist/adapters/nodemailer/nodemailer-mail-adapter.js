b7ff1ae7200a2c12c59f2dceed3b562d9c7408e1
Isabelle Bussmann <isabellebussmann@hotmail.com>
2022-05-17 20:31:55 -0300

    Back-end settings

---
+++ server/dist/adapters/nodemailer/nodemailer-mail-adapter.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NodemailerMailAdapter = void 0;
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const transport = nodemailer_1.default.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "d5fa9a0f66c8ce",
+        pass: "a5226409d96dd5"
+    }
+});
+class NodemailerMailAdapter {
+    async sendMail({ subject, body }) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Isabelle Bussmann <isabellebussmann@hotmail.com>',
+            subject,
+            html: body,
+        });
+    }
+    ;
+}
+exports.NodemailerMailAdapter = NodemailerMailAdapter;
---
+++ server/dist/use-cases/submit-feedback-use-case.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SubmitFeedbackUseCase = void 0;
+class SubmitFeedbackUseCase {
+    constructor(feedbacksRepository, mailAdapter) {
+        this.feedbacksRepository = feedbacksRepository;
+        this.mailAdapter = mailAdapter;
+        this.feedbacksRepository = feedbacksRepository;
+    }
+    async execute(request) {
+        const { type, comment, screenshot } = request;
+        if (!type) {
+            throw new Error('Type is required.');
+        }
+        if (!comment) {
+            throw new Error('Comment is required.');
+        }
+        if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+            throw new Error('Invalid screenshot format.');
+        }
+        await this.feedbacksRepository.create({
+            type,
+            comment,
+            screenshot,
+        });
+        await this.mailAdapter.sendMail({
+            subject: 'Novo feedback',
+            body: [
+                `<div styles="font-family: sans-serif; font-size: 16px; color: #111;">`,
+                `<p>Tipo do feedback: ${type}</p>`,
+                `<p>Comentário: ${comment}</p>`,
+                screenshot ? `<img src= "${screenshot}" style="width: 100%;  height: auto;"/>` : ``,
+                `</div>`
+            ].join('\n')
+        });
+    }
+}
+exports.SubmitFeedbackUseCase = SubmitFeedbackUseCase;
